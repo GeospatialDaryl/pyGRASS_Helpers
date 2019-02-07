c745bfd351c0c7557cbe352c818b21d52706ff01
Daryl Van Dyke <GeospatialDaryl@users.noreply.github.com>
2019-02-06 17:13:48 -0800

    export

---
+++ rasterHelpers.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+
+import grass.script as gscript
+from grass.pygrass.modules import Module
+
+def ExportTiff(inRas, outPath = "L:\\_04_LiDAR_Workspace\\Trinity_Geomorph\\"):
+    modN = "r.out.gdal"
+    outName = inRas.split("@")[0]
+    print(outName)
+    Module(modN,
+        input="rasTrinityDetrend_fomrms@DetrendSurface", 
+        output=outPath+outName+".tif", 
+        format='GTiff',
+        overwrite = True)
